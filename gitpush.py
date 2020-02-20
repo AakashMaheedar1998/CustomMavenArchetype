0fd2e10601a47b67114b0fe5e887a769a8b4f711
Aakash Maheedar <58288773+Aakash-Maheedar@users.noreply.github.com>
2020-02-20 13:00:57 +0530

    My first File

---
+++ gitpush.py
@@ -0,0 +1,20 @@
+import os
+
+
+
+cmd1 = 'git init'
+os.system(cmd1)
+cmd2_2 ='git status'
+os.system(cmd2_2)
+cmd2 = 'git add  .'
+os.system(cmd2)
+
+cmd3 = 'git commit -m "My first File"'
+os.system(cmd3)
+cmd4 = 'git remote add origin https://github.com/AakashMaheedar/Test'
+os.system(cmd4)
+os.system(cmd2_2)
+cmd5 = 'git pull origin master'
+os.system(cmd5)
+cmd6 = 'git push -u origin master'
+os.system(cmd6)
