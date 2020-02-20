b45e2c2e6cd013cfb2ae6288d4c266ea9ba2418f
Aakash Maheedar <58288773+Aakash-Maheedar@users.noreply.github.com>
2020-02-20 13:18:28 +0530

    First commit

--- gitpush.py
+++ gitpush.py
@@ -8,7 +8,7 @@ cmd2 = 'git status'
 os.system(cmd2)
 cmd3 = 'git add .'
 os.system(cmd3)
-cmd4 = "git commit -m 'First commit' "
+cmd4 = 'git commit -m "First commit" '
 
 os.system(cmd4)
 cmd5 = 'git status'
@@ -17,5 +17,5 @@ cmd6 = 'git remote add origin https://github.com/AakashMaheedar/Test'
 os.system(cmd6)
 cmd6 = 'git push -u origin master'
 os.system(cmd6)
-cmd6 = 'git status'
-os.system(cmd6)
+cmd7 = 'git status'
+os.system(cmd7)
